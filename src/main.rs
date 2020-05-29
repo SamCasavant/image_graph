c64ef99e29387afbe776b4736408a1493e86c437
Samuel Casavant <samuel.j.casavant@gmail.com>
2020-05-29 13:04:14 -0400

    Added merge nodes function

--- src/main.rs
+++ src/main.rs
@@ -8,81 +8,122 @@ use petgraph::dot::{Dot, Config};
 
 use std::collections::HashMap;
 
-// #[derive(Hash, Eq, PartialEq, Debug)]
-// struct node_map {
-//     coordinates: (u32, u32),
-//     node: u32,
-// }
 
-// impl node_map {
-//     fn new(coordinates: (u32, u32), node: u32) -> node_map {
-//         node_map {coordinates: coordinates, node: node }
-//     }
-// }
+type G = petgraph::Graph<(u32, u32, image::Rgba<u8>), u32, petgraph::Undirected, u32>;
+type N = petgraph::prelude::NodeIndex;
 
 fn main() {
-    let img = image::open("data/small.jpg").unwrap();
-    let mut img_graph: petgraph::Graph<(u32, u32, image::Rgba<u8>), f64, petgraph::Undirected, u32> = Graph::new_undirected();
-    let scan_range: u32 = 4;
+    let img = image::open("data/letter_p.jpg").unwrap();
+    let mut img_graph: G = Graph::new_undirected();
+    let scan_range: u32 = 5;
+    let threshhold: f64 = 30.0;
+    let dimension_factor = 10;
+    let color_factor = 1;
     let mut node_map = HashMap::new();
     for pixel in img.pixels(){
+        //Generate graph from pixel data
         let base_node = img_graph.add_node(pixel);
-        //println!("{:?}", base_node);
-        let threshhold: f64 = 30.0;
-        let dimension_factor = 1;
-        let color_factor = 1;
-        let (base_x_u32, base_y_u32, base_rgba) = pixel;
-        let (base_x, base_y) = (base_x_u32 as i64, base_y_u32 as i64);
-        let (base_r, base_g, base_b) = (base_rgba[0] as i64, base_rgba[1] as i64, base_rgba[2] as i64);
-        node_map.entry((base_x_u32, base_y_u32)).or_insert(base_node);
-        let mut neighbors = vec![];//u32, 3, scan_range.pow(2)-1];
+        let (base_x, base_y, base_rgba) = pixel;
+        let (base_r, base_g, base_b) = (base_rgba[0], base_rgba[1], base_rgba[2]);
+        node_map.entry((base_x, base_y)).or_insert(base_node); 
+        let mut neighbors = vec![];
         let mut min_x = 0;
         let mut min_y = 0;
         let mut max_x = img.width();
         let mut max_y = img.height();
-        if base_x_u32 > scan_range {
-            min_x = base_x_u32 - scan_range;
+        if base_x > scan_range {
+            min_x = base_x - scan_range;
         }
-        if base_y_u32 > scan_range {
-            min_y = base_y_u32 - scan_range;
+        if base_y > scan_range {
+            min_y = base_y - scan_range;
         }
-        if base_x_u32 + scan_range < img.width(){
-            max_x = base_x_u32 + scan_range;
+        if base_x + scan_range < img.width(){
+            max_x = base_x + scan_range;
         }
-        if base_y_u32 + scan_range < img.height(){
-            max_y = base_y_u32 + scan_range;
+        if base_y + scan_range < img.height(){
+            max_y = base_y + scan_range;
         }
         for x in min_x..max_x{
             for y in min_y..max_y{
                 let neighbor = node_map.get(&(x,y));
-                if neighbor != None{
-                    neighbors.push(neighbor.unwrap())
+                if neighbor != None && (x, y) != (base_x, base_y){
+                    neighbors.push(neighbor.unwrap());
                 }
             }
         }
         for link_node in neighbors{
-            let (link_x_u32, link_y_u32, link_rgba) = img_graph.node_weight(*link_node).unwrap();
-            let (link_x, link_y) = (*link_x_u32 as i64, *link_y_u32 as i64);
-            let (link_r, link_g, link_b) = (link_rgba[0] as i64, link_rgba[1] as i64, link_rgba[2] as i64);
+            if img_graph.contains_edge(base_node, *link_node) == false{
+                let (link_x, link_y, link_rgba) = img_graph.node_weight(*link_node).unwrap();
+                let (link_r, link_g, link_b) = (link_rgba[0], link_rgba[1], link_rgba[2]);
 
-            if (((base_x-link_x).pow(2) + (base_y-link_y).pow(2)) as f64).sqrt() < 2.0{
-                let raw_distance =  (base_x - link_x).pow(2) * dimension_factor +  
-                                    (base_y - link_y).pow(2) * dimension_factor +
-                                    (base_r - link_r).pow(2) * color_factor +
-                                    (base_g - link_g).pow(2) * color_factor + 
-                                    (base_b - link_b).pow(2) * color_factor;
+                let raw_distance =  ((base_x as i64 - *link_x as i64) * dimension_factor).pow(2) +
+                                    ((base_y as i64 - *link_y as i64) * dimension_factor).pow(2) +
+                                    ((base_r as i64 - link_r as i64) * color_factor).pow(2) +
+                                    ((base_g as i64 - link_g as i64) * color_factor).pow(2)  + 
+                                    ((base_b as i64 - link_b as i64) * color_factor).pow(2);
                 let linear_distance = (raw_distance as f64).sqrt();
                 if linear_distance < threshhold{
-                    img_graph.add_edge(base_node, *link_node, linear_distance);
+                    let linear_weight = (10000.0/linear_distance) as u32;
+                    img_graph.add_edge(base_node, *link_node, linear_weight);
                 }
             }
         }
     }
+
+    //Merge nodes that have high shared connectivity
+    let mut removed_nodes: Vec<N> = vec![];
+    for base in img_graph.node_indices(){
+        if removed_nodes.contains(&base) == false{
+            let base_neighbors: Vec<N> = img_graph.neighbors(base).collect();
+            for link in &base_neighbors{
+                if removed_nodes.contains(link) == false{
+                    let link_neighbors: Vec<N> = img_graph.neighbors(*link).collect();
+                    let shared_neighbors: Vec<&N> = base_neighbors.iter().filter(|n| link_neighbors.contains(n)).collect();
+                    println!("base_neighbors: {:?}\nlink_neighbors: {:?}\nshared neighbors: {:?}", base_neighbors.len(), link_neighbors.len(), shared_neighbors.len());
+                    //if shared_neighbors.len() > 0{
+                        for link_neighbor in &link_neighbors{
+                            if shared_neighbors.contains(&link_neighbor) == false && link_neighbor != &base{
+                                if img_graph.find_edge_undirected(base, *link_neighbor) == None{
+                                    let edge = img_graph.find_edge_undirected(*link, *link_neighbor).unwrap();
+                                    img_graph.add_edge(base, *link_neighbor, *img_graph.edge_weight(edge.0).unwrap());
+                                }
+                            }
+                        }
+                        img_graph.remove_node(*link);
+                        removed_nodes.push(*link);
+                    //}
+                }
+            }
+        }
+    }
+
+
     //println!("Graph: {:?}", debug_array);
     use std::fs::File;
     use std::io::Write;
-    let mut f = File::create("out.dot").unwrap();
+    let mut f = File::create("test.dot").unwrap();
     //println!("{:?}", Dot::with_config(&img_graph, &[Config::EdgeNoLabel]));
+    //let condensed_graph = condensation(img_graph, false);
     let output = format!("{:?}", Dot::with_config(&img_graph, &[Config::NodeIndexLabel]));
     f.write_all(&output.as_bytes());
+}
+
+
+fn merge_nodes(mut g: petgraph::Graph<(u32, u32, image::Rgba<u8>), u32, petgraph::Undirected>, node_1: N, node_2: N){ 
+    //Move edges from node_2 onto node_1, sum edge weight on shared edges, and delete node_2 from graph.
+    let node_2_neighbors: Vec<N> = g.neighbors(node_2).collect();
+    for node_2_neighbor in node_2_neighbors{
+        let edge = g.find_edge_undirected(node_1, node_2_neighbor);
+        let edge_2 = g.find_edge_undirected(node_2, node_2_neighbor).unwrap();
+        let edge_2_weight = *g.edge_weight(edge_2.0).unwrap();
+        if edge != None{
+            let edge_weight = g.edge_weight_mut(edge.unwrap().0).unwrap();
+            *edge_weight += edge_2_weight;
+        }else{
+            if node_2_neighbor != node_1{
+                g.add_edge(node_1, node_2_neighbor, edge_2_weight);
+            }
+        }
+    }
+    g.remove_node(node_2);
 }
---
+++ target/debug/image_graph.d
@@ -0,0 +1,1 @@
+/home/sam/Projects/image_graph/target/debug/image_graph: /home/sam/Projects/image_graph/src/main.rs
